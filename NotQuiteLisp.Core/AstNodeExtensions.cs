ba450330d49d19f13a5936263e7c8f3afc29c2c4
Philip Laureano <philip.laureano@gmail.com>
2015-05-05 07:38:46 +1000

    Added a Descend method for traversing a tree with a callback function

--- NotQuiteLisp.Core/AstNodeExtensions.cs
+++ NotQuiteLisp.Core/AstNodeExtensions.cs
@@ -4,6 +4,8 @@ using NotQuiteLisp.AST;
 
 namespace NotQuiteLisp.Core
 {
+    using System;
+
     using NotQuiteLisp.AST.Interfaces;
 
     public static class AstNodeExtensions
@@ -11,19 +13,30 @@ namespace NotQuiteLisp.Core
         public static IEnumerable<INode<T>> Descendants<T>(this INode<T> node, int? maxDepth = null)
         {
             var descendants = new List<INode<T>>();
-            AddDescendants(node, descendants, 0, maxDepth);
+            node.Descend((currentDepth, currentNode)=> descendants.Add(currentNode));
 
             return descendants;
         }
 
-        private static void AddDescendants<T>(this INode<T> node, ICollection<INode<T>> descendants, int currentDepth, int? maxDepth = null)
+        public static void Descend<T>(this INode<T> node,
+            Action<int, INode<T>> callbackFunc,
+            int? maxDepth = null)
+        {
+            node.Descend(callbackFunc, 0, maxDepth);
+        }
+
+        private static void Descend<T>(
+            this INode<T> node,
+            Action<int, INode<T>> callbackFunc,
+            int currentDepth,
+            int? maxDepth = null)
         {
             foreach (var child in node.Children)
             {
-                AddDescendants(child, descendants, currentDepth + 1, maxDepth);
+                child.Descend(callbackFunc, currentDepth + 1, maxDepth);
 
                 if (maxDepth == null || (currentDepth < maxDepth.Value))
-                    descendants.Add(child);
+                    callbackFunc(currentDepth, child);
             }
         }
     }
